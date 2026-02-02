62ce144cb915bcecf9a710710be1320e42444f84
Leah Rouse Cruzo <lrcruzo.code@gmail.com>
2026-02-03 00:10:21 +0800

    Added button functions for logout, profile ang messages

--- app/src/main/java/com/example/studypals/ProfileActivity.kt
+++ app/src/main/java/com/example/studypals/ProfileActivity.kt
@@ -1,14 +1,22 @@
 package com.example.studypals
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // This links the Kotlin code to the profile.xml design
         setContentView(R.layout.profile)
 
+        val btnLogout  = findViewById<Button>(R.id.btnLogout)
+
+        //logout
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
+
     }
+}
