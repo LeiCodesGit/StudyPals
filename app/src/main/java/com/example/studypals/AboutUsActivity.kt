01549890f7e65fb8dcbf9bcffb22b7b8bf64fabb
sami <sophiamanagbanag@gmail.com>
2026-03-18 13:35:56 +0800

    ABOUT US AND PROFILE EDIT

---
+++ app/src/main/java/com/example/studypals/AboutUsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.studypals
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+
+        findViewById<ImageButton>(R.id.btnBack).setOnClickListener {
+            finish()
+        }
+    }
+}
